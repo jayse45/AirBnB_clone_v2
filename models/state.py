c56fd536e93e5f8db25fecf361cfd504687ec11b
jayse45 <68971541+jayse45@users.noreply.github.com>
2021-12-23 14:41:47 +0000

    More corrections

--- models/state.py
+++ models/state.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
-"""
-This is the state class without marge
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.orm import relationship, backref
-import models
-from os import getenv
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """
-    This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class / table model"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City",
-                              backref="state",
-                              cascade="all, delete")
-
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
+        name = ''
+
         @property
         def cities(self):
-            """
-            Returns the list of City instances with
-            state_id == current State.id
-            """
-            instance_list = []
-            for key, obj in models.storage.all().items():
-                if obj.__class__.__name__ == 'City':
-                    if obj.state_id == self.id:
-                        instance_list.append(obj)
-            return instance_list
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
